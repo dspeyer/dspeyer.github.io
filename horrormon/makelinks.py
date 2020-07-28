787ccb1824965dd3c83fcb0f7e170a7218045311
Daniel Speyer <dspeyer@gmail.com>
2020-07-27 23:53:41 -0400

    horrormon

---
+++ horrormon/makelinks.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+from glob import glob
+from collections import defaultdict
+from re import sub
+from itertools import count
+
+fns = glob('*_*_*.html')
+raw = []
+for fn in fns:
+    try:
+        author,tick,length = fn[:-5].split('_')
+    except ValueError:
+        print('Skipping "%s"'%fn[:-5])
+        continue
+    tick = int(tick)
+    length = int(length)
+    f = open(fn,'r')
+    title = 'untitled'
+    stub = False
+    for l in f:
+        if '<!--small-->' in l:
+            stub = True
+        if 'h1' in l:
+            title = sub('<[^>]*>','',l.strip())
+            break
+    f.close()
+    raw.append({'author':author,'tick':tick,'length':length,'title':title,'fn':fn,'stub':stub})
+
+    
+bytime = defaultdict(list)
+bytimeauth = {}
+
+for x in raw:
+    for i in range(x['length']):
+        bytime[x['tick']+i].append(x)
+        bytimeauth[(x['tick']+i,x['author'])] = x
+
+for x in raw:
+    f = open(x['fn'],'a')
+    f.write('</div><hr><div class=content><h3>Navigation:</h3><ul>')
+    
+    search = True
+    if (x['tick']-1,x['author']) in bytimeauth:
+        p = bytimeauth[(x['tick']-1,x['author'])]
+        if p['stub']:
+            f.write('<li><b>Previously:</b> <a href=%s>%s</a> [stub]'%
+                    (p['fn'],p['title']))
+        else:
+            f.write('<li><b>Previously:</b> <a href=%s>%s</a>'%
+                    (p['fn'],p['title']))
+            search = False
+    if search:
+        for i in count(2,1):
+            if (x['tick']-i,x['author']) in bytimeauth:            
+                p = bytimeauth[(x['tick']-i,x['author'])]
+                if not p['stub']:
+                    f.write('<li><b>Previously with substance in this Account:</b> <a href=%s>%s</a>'%(p['fn'],p['title']))
+                    break
+            if x['tick']-i not in bytime:
+                break
+        for a in ['sarah','otto','citrine','historian']:
+            if (x['tick']-1,a) in bytimeauth:
+                p = bytimeauth[(x['tick']-1,a)]
+                if not p['stub']:
+                    f.write('<li><b>Immediately Previously by %s:</b> <a href=%s>%s</a>'%(a.title(), p['fn'],p['title']))
+                    break
+
+    seen = { x['fn'] }
+    for i in range(x['length']):
+        for y in bytime[x['tick']+i]:
+            if y['fn'] in seen or y['stub']:
+                continue
+            f.write('<li><b>Concurrently by %s:</b> <a href=%s>%s</a>'%(y['author'].title(), y['fn'],y['title']))
+            seen.add(y['fn'])
+
+    # TODO: unify
+    search = True
+    if (x['tick']+x['length'],x['author']) in bytimeauth:
+        p = bytimeauth[(x['tick']+x['length'],x['author'])]
+        if p['stub']:
+            f.write('<li><b>Next:</b> <a href=%s>%s</a> [stub]'%(p['fn'],p['title']))
+        else:
+            f.write('<li><b>Next:</b> <a href=%s>%s</a>'%(p['fn'],p['title']))
+            search = False
+    if search:
+        for i in count(1,1):
+            if (x['tick']+x['length']+i,x['author']) in bytimeauth:            
+                p = bytimeauth[(x['tick']+x['length']+i,x['author'])]
+                if not p['stub']:
+                    f.write('<li><b>Next substantive in this Account:</b> <a href=%s>%s</a>'%(p['fn'],p['title']))
+                    break
+            if x['tick']+x['length']+i not in bytime:
+                break
+        for a in ['sarah','otto','citrine','historian']:
+            if (x['tick']+x['length']+1,a) in bytimeauth:
+                p = bytimeauth[(x['tick']+x['length']+1,a)]
+                if not p['stub']:
+                    f.write('<li><b>Immediately Next by %s:</b> <a href=%s>%s</a>'%(a.title(), p['fn'],p['title']))
+                    break
+
+    f.write('<li><b><a href=index.html>Index</a></b>')
+    f.write('</ul></div></body></html>')
+    f.close()
---
+++ horrormon/script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+rm *.html tmp/*.html
+
+for i in raw/*.txt; do
+    pandoc $i > tmp/`basename $i .txt`.html
+done
+
+for i in raw/*.html; do
+    perl -e '$b=0;while(<>){if (/body/) {$b=1;} elsif ($b and not /html/) {print;}}' < $i > tmp/`basename $i`;
+done
+
+echo '<html><head><title>HorrorMon Index</title><link rel=stylesheet href=theme.css></head><body>' > index.html
+echo '<table>' >> index.html
+echo "<thead><tr><!--td class=small>#</td--><td class=sarah>Sarah's Diary</td><td class=otto>Otto's Log</td><td class=citrine>Citrine's Journal</td><td class=historian>Additional Texts</td></tr></thead>" >> index.html
+for i in `ls tmp/*_*.html | cut -d _ -f 2 | sort -n | uniq`; do
+    echo "<tr><!--td class=small>${i}</td-->" >> index.html
+    if test $i -eq '-1'; then
+        echo "<td class=invis colspan=3></td>" >> index.html
+    fi
+    if test $i -eq 13; then
+        echo "<td class=invis style='border:none' rowspan=4></td>" >> index.html
+    fi
+    for n in sarah otto citrine marianna jacqueline historian; do
+        FN=`echo tmp/${n}_${i}_*`
+        if test -e $FN; then
+            RS=`echo $FN | sed 's/.*_//g' | sed 's/.html//'`
+            T=`grep -i '<h1' $FN | head -n 1 | sed 's/<[^>]*>//g'`
+            if echo $n | egrep -v 'sarah|otto|citrine' >/dev/null; then
+                T="<div class=author>${n}'s account:</div> $T";
+            fi
+            if test `wc -l $FN | cut -f 1 -d ' '` -gt 1; then
+                HREF="href=`basename $FN`"
+                CL="$n"
+            else
+                HREF=""
+                CL="small"
+            fi
+            echo "<td rowspan=${RS} class=${CL}><a $HREF >${T}</a></td>" >> index.html
+            echo "<html><head><title>$T - ${n^} - HorrorMon</title><link rel=stylesheet href=theme.css></head><!--${CL}--><body class=$n><div class=content>" > `basename $FN`
+            cat $FN >> `basename $FN`
+        fi
+    done
+    echo "</tr>" >> index.html
+done
+echo "</table></body></html>" >> index.html
+
+./makelinks.py
